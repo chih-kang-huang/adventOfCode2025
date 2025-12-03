a5c536019953a0c129c7277dbb401a3d2eb40ab6
Picodon <chih-kang.huang@hotmail.com>
2025-12-03 12:37:16 +0100

    Day3 - 1/2

---
+++ Day3/D3_solution.py
@@ -0,0 +1,25 @@
+def maxJoltage(s):
+    # s = string of numbers
+    digit1 = float("-inf")
+    digit2 = float("-inf")
+    for c in s[:-1]: 
+        c = int(c)
+        if c > digit1: 
+            digit1 = c 
+            digit2 = float("-inf") # reset digit2
+        elif c > digit2: 
+            digit2 = c
+    # case where digit2 not affected by the end
+    digit2 = max(digit2, int(s[-1]))
+
+    return digit1*10 + digit2
+
+res = 0
+with open("input.txt", "r") as f:
+    for s in f:
+        s = s.strip() # remove trailing lines
+        print(f"{maxJoltage(s)} and {s}")
+        res += maxJoltage(s)
+
+print(res)
+
