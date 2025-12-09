e371c949b089d93140ef0152c61840d79b32baa0
Picodon <chih-kang.huang@hotmail.com>
2025-12-10 00:46:02 +0100

    Day 9 - 1

---
+++ Day9/D9_solution.py
@@ -0,0 +1,20 @@
+pointList = []
+with open('input.txt') as f: 
+    for line in f: 
+        a, b = line.strip().split(',')
+        pointList.append((int(a),int(b)))
+
+pointList.sort()
+
+def area(pt1, pt2): 
+    return (abs(pt1[0] - pt2[0])+1) * (abs(pt1[1] - pt2[1])+1)
+
+
+
+res = 0
+
+for i in range(len(pointList)): 
+    for j in range(len(pointList)):
+        res = max(res, area(pointList[i], pointList[j]))
+print(res)
+
